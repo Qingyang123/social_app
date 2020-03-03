8a52fed3f33c83b1513ee0bba2a604a57258288f
Qingyang Liu <qingyang@qingyangs-mbp.dyndns.rice.edu>
2020-03-03 16:01:41 -0600

    pages and basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
+// Components
+import Navbar from './components/Navbar';
+
+// Pages
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
+
+// CSS
 import './App.css';
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			light: '#33c9dc',
+			main: '#00bcd4',
+			dark: '#008394',
+			contrastText: '#fff'
+		},
+		secondary: {
+			light: '#ff6333',
+			main: '#ff3d00',
+			dark: '#b22a00',
+			contrastText: '#fff'
+		}
+	},
+	typography: {
+		useNextVariants: true
+	}
+})
 
 function App() {
 	return (
-		<div className="App">
-		</div>
+		<MuiThemeProvider theme={theme}>
+			<div className="App">
+				<BrowserRouter>
+					<Navbar/>
+					<div className='container'>
+						<Switch>
+							<Route exact path='/' component={Home}/>
+							<Route exact path='/login' component={Login}/>
+							<Route exact path='/signup' component={Signup}/>
+						</Switch>
+					</div>
+				</BrowserRouter>
+			</div>
+		</MuiThemeProvider>
 	);
 }
 
---
+++ client/src/pages/home.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+// MUI
+import Grid from '@material-ui/core/Grid';
+
+class Home extends Component {
+
+    state = {
+        screams: null
+    }
+
+    componentDidMount() {
+        axios.get('/screams')
+            .then(res => {
+                console.log(res.data)
+                this.setState({
+                    screams: res.data
+                })
+            })
+            .catch(err => console.log(err));
+    }
+    render() {
+        let recentScreamsMarkup = this.state.screams ? (
+            this.state.screams.map(scream => <p>{ scream.body }</p>)
+        ) : <p>Loading ...</p>
+        return (
+            <Grid container>
+                <Grid item sm={8} xs={12}>
+                    { recentScreamsMarkup }
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile ...</p>
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default Home;
